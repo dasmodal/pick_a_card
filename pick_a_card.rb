3a5fdf71b1c290f59e5908748e37bbfc6e7608d5
dasmodal <das.modal@gmail.com>
2022-04-17 11:15:47 +0700

    add symbols of cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,7 @@
 # Hi
 # Массивы с наборами мастей и достоинств
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+suits = ['♦', '♥', '♣', '♠'] # Буби, Черви, Крести, Пики
 
-puts "#{values.sample} of #{suits.sample}"
+
+puts "#{values.sample}#{suits.sample}"
